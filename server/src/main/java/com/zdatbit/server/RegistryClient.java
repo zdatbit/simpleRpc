a96d6aed01638deb875e388cb1885fc2ef8327bc
zhangdi21 <zhangdi21@58.com>
2020-07-08 20:00:33 +0800

    代码优化

--- server/src/main/java/com/zdatbit/server/RegistryClient.java
+++ server/src/main/java/com/zdatbit/server/RegistryClient.java
@@ -1,45 +1,63 @@
-//package com.zdatbit.server;
-//
-//import io.netty.bootstrap.Bootstrap;
-//import io.netty.channel.Channel;
-//import io.netty.channel.ChannelInitializer;
-//import io.netty.channel.ChannelPipeline;
-//import io.netty.channel.nio.NioEventLoopGroup;
-//import io.netty.channel.socket.SocketChannel;
-//import io.netty.channel.socket.nio.NioSocketChannel;
-//import io.netty.handler.codec.string.StringDecoder;
-//import io.netty.handler.codec.string.StringEncoder;
-//
-//public class RegistryClient {
-//
-//    private static Channel ch;
-//    private static Bootstrap bootstrap;
-//
-//
-//    public void registry() {
-//        NioEventLoopGroup workGroup = new NioEventLoopGroup();
-//        try {
-//            bootstrap = new Bootstrap();
-//            bootstrap.group(workGroup)
-//                    .channel(NioSocketChannel.class)
-//                    .handler(new ChannelInitializer<SocketChannel>() {
-//                        @Override
-//                        protected void initChannel(SocketChannel ch) throws Exception {
-//                            ChannelPipeline pipeline = ch.pipeline();
-//                            pipeline.addLast(new StringDecoder());
-//                            pipeline.addLast(new StringEncoder());
-//                            pipeline.addLast(new ClientHeartbeatHandler());
-//                        }
-//                    });
-//
-//            // 连接服务器
-//            doConnect();
-//
-//
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } finally {
-//            workGroup.shutdownGracefully();
-//        }
-//    }
-//}
+package com.zdatbit.server;
+
+import com.alibaba.fastjson.JSON;
+import com.zdatbit.common.HeartBeat;
+import com.zdatbit.common.utils.CommonPool;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.TimeUnit;
+
+public class RegistryClient{
+    private EventLoopGroup group = new NioEventLoopGroup();
+    private Bootstrap client = new Bootstrap();
+    private String ipAddress;
+    private int port;
+    private HeartBeat heartBeat;
+
+    public RegistryClient(){
+
+    }
+
+    public RegistryClient(String ipAddress,int port,HeartBeat heartBeat){
+        this.ipAddress = ipAddress;
+        this.port = port;
+        this.heartBeat = heartBeat;
+    }
+
+    public void register(){
+        try {
+            client.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        public void initChannel(SocketChannel ch)
+                                throws Exception {
+                            ch.pipeline().addLast(new StringDecoder(CharsetUtil.UTF_8));
+                            ch.pipeline().addLast(new StringEncoder(CharsetUtil.UTF_8));
+                            ch.pipeline().addLast(new HeartBeatSend());
+                        }
+                    });
+
+            ChannelFuture f = client.connect(ipAddress, port).sync();
+            CommonPool.scheduledExecutorService.scheduleAtFixedRate(()->{
+                heartBeat.setLastUpdate(System.currentTimeMillis());
+                f.channel().writeAndFlush(JSON.toJSONString(heartBeat));
+            },10,5, TimeUnit.SECONDS);
+            f.channel().closeFuture().sync();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+}
