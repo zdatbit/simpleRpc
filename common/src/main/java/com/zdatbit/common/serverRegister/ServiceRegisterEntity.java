25b58055fe6217c9479a1314407012396c6a3b7c
zhangdi <zdatbit@163.com>
2021-03-16 18:25:00 +0800

    服务信息解析成功

--- common/src/main/java/com/zdatbit/common/serverRegister/ServiceRegisterEntity.java
+++ common/src/main/java/com/zdatbit/common/serverRegister/ServiceRegisterEntity.java
@@ -3,8 +3,9 @@ package com.zdatbit.common.serverRegister;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
-import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 @Data
 @Accessors(chain = true)
@@ -18,7 +19,21 @@ public class ServiceRegisterEntity {
     /**方法列表**/
     private List<Methods> methodsList;
     /**IP地址**/
-    private List<String> ips;
+    private Set<String> ips;
     /**服务端口**/
     private String port;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServiceRegisterEntity that = (ServiceRegisterEntity) o;
+        return Objects.equals(serviceName, that.serviceName) && Objects.equals(serviceInter, that.serviceInter) && Objects.equals(serviceImpl, that.serviceImpl) && Objects.equals(methodsList, that.methodsList) && Objects.equals(ips, that.ips) && Objects.equals(port, that.port);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serviceName, serviceInter, serviceImpl, methodsList, ips, port);
+    }
 }
